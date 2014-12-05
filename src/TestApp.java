e179ea33b12d4daf62057fa4b4d21aa20f182118
Dwegiel <dwegiel@gmail.com>
2014-12-04 23:49:40 -0600

    More features

--- src/TestApp.java
+++ src/TestApp.java
@@ -19,70 +19,113 @@ public class TestApp {
 		Word myWord = new Word("accomplished",3,5,"adjective","settled securely and unconditionally"); //a word contained in our data file (should be found)
 		Word falseWord = new Word("acco",4,8,"noun","secure"); //a word not contained in our data file (should not be found)
 		
-		//testing the search function (isThere) for a word present in the Array
+		System.out.println("* Search function (isThere) for a word present in the Array");
+		System.out.println();
 		System.out.println("We are searching for:");
 		System.out.println(myWord.toString());
 		System.out.println("Found at position: " + wordArr.isThere(myWord));
 		System.out.println();
 		System.out.println();
 		
-		//testing the search function (isThere) for a word not present in the Array
+		System.out.println("* Search function (isThere) for a word not present in the Array");
+		System.out.println();
 		System.out.println("We are searching for:");
 		System.out.println(falseWord.toString());
 		System.out.println("Found at position: " + wordArr.isThere(falseWord));
 		System.out.println();
 		System.out.println();
 		
-		//testing the sort (by word length) function for the array
+		System.out.println("* Sort (by word length) function for the array");
+		System.out.println();
 		System.out.println("Prior to sorting by length:");
 		System.out.println("First word:\n" + wordArr.getAtPos(0).toString());
 		System.out.println("Last word:\n" + wordArr.getAtPos(wordArr.getIndex()-1).toString());
+		System.out.println();
 		System.out.println("Sorting by length...");
 		wordArr.sort();
+		System.out.println();
 		System.out.println("After sorting by length:");
 		System.out.println("First word:\n" + wordArr.getAtPos(0).toString());
 		System.out.println("Last word:\n" + wordArr.getAtPos(wordArr.getIndex()-1).toString());
 		System.out.println();
 		System.out.println();
 		
-		//adding a new word to the array
+		System.out.println("* Adding a new word to the array");
+		System.out.println();
 		System.out.println("Current index: " + wordArr.getIndex());
 		System.out.println("Word at current index-1 position:");
 		System.out.println(wordArr.getAtPos(wordArr.getIndex()-1).toString());
+		System.out.println();
 		System.out.println("Adding a new word at the index with the following properties:");
 		System.out.println(falseWord.toString());
 		wordArr.add(falseWord);
+		System.out.println();
 		System.out.println("Current index: " + wordArr.getIndex());
 		System.out.println("Word at current index-1 position:");
 		System.out.println(wordArr.getAtPos(wordArr.getIndex()-1).toString());
 		System.out.println();
 		System.out.println();
 		
-		//removing the first word from the array, by position
+		System.out.println("* Inserting a new word to the array at a specific position (5)");
+		System.out.println();
+		System.out.println("Current index: " + wordArr.getIndex());
+		System.out.println("Word at position 4:");
+		System.out.println(wordArr.getAtPos(4).toString());
+		System.out.println();
+		System.out.println("Word at position 5:");
+		System.out.println(wordArr.getAtPos(5).toString());
+		System.out.println();
+		System.out.println("Word at position 6:");
+		System.out.println(wordArr.getAtPos(6).toString());
+		System.out.println();
+		System.out.println("Adding a new word at position 5 with the following properties:");
+		System.out.println(falseWord.toString());
+		wordArr.insert(5,falseWord);
+		System.out.println();
+		System.out.println("Current index: " + wordArr.getIndex());
+		System.out.println("Word at position 4:");
+		System.out.println(wordArr.getAtPos(4).toString());
+		System.out.println();
+		System.out.println("Word at position 5:");
+		System.out.println(wordArr.getAtPos(5).toString());
+		System.out.println();
+		System.out.println("Word at position 6:");
+		System.out.println(wordArr.getAtPos(6).toString());
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("* Removing the first word from the array, by position");
+		System.out.println();
 		System.out.println("Current index: " + wordArr.getIndex());
 		System.out.println("First word in the array:");
 		System.out.println(wordArr.getAtPos(0).toString());
+		System.out.println();
 		System.out.println("Removing the first word by position...");
 		wordArr.delete(0);
+		System.out.println();
 		System.out.println("Current index: " + wordArr.getIndex());
 		System.out.println("(New) First word in the array:");
 		System.out.println(wordArr.getAtPos(0).toString());
 		System.out.println();
 		System.out.println();
 		
-		//removing the last word from the array, by object
+		System.out.println("* Removing the last word from the array, by object");
+		System.out.println();
 		System.out.println("Current index: " + wordArr.getIndex());
 		System.out.println("Word at current index-1 position:");
 		System.out.println(wordArr.getAtPos(wordArr.getIndex()-1).toString());
+		System.out.println();
 		System.out.println("Removing the last word by object...");
 		wordArr.delete(falseWord);
+		System.out.println();
 		System.out.println("Current index: " + wordArr.getIndex());
 		System.out.println("Word at current index-1 position:");
 		System.out.println(wordArr.getAtPos(wordArr.getIndex()-1).toString());
 		System.out.println();
 		System.out.println();
 		
-		//Menu testing TO ADD in the output file
+		//Menu testing	
 		
 		//Polymorphism testing
 		
